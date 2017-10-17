0b3c60e380bb094d4485c22366c8a25af5c8f793
Roberval Zamperline <rzamperline@cambe.pr.gov.br>
2017-10-17 09:21:14 -0200

    first commit

---
+++ src/main/resources/db/migration/V02__criar_e_registar_categorias.sql
@@ -0,0 +1,15 @@
+CREATE TABLE public.categoria
+(
+  id bigint DEFAULT nextval('categoria_id_seq') NOT NULL,
+  nome VARCHAR(255) NOT NULL,
+  CONSTRAINT categoria_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE  public.categoria OWNER TO postgres;
+  
+INSERT INTO public.categoria (nome) VALUES('Lazer');
+INSERT INTO public.categoria (nome) VALUES('Alimentação');
+INSERT INTO public.categoria (nome) VALUES('Supermercado');
+INSERT INTO public.categoria (nome) VALUES('Farmácia');
