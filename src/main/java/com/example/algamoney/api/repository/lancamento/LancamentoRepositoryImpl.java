bb530335991d9ef85567d3fc8383ab88c3b80efa
Roberval Zamperline <rzamperline@cambe.pr.gov.br>
2017-10-19 08:19:47 -0200

    final

--- src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
@@ -16,9 +16,12 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
+import com.example.algamoney.api.model.Categoria_;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.model.Lancamento_;
+import com.example.algamoney.api.model.Pessoa_;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import com.example.algamoney.api.repository.projection.ResumoLancamento;
 
 public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 
@@ -39,26 +42,53 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 		
 		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
 	}
+	
 
-	private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder, Root<Lancamento> root) {
+	@Override
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<ResumoLancamento> criteria = builder.createQuery(ResumoLancamento.class);
+		Root<Lancamento> root = criteria.from(Lancamento.class);
+		
+		criteria.select(builder.construct(ResumoLancamento.class
+				, root.get(Lancamento_.id), root.get(Lancamento_.descricao)
+				, root.get(Lancamento_.dataVencimento), root.get(Lancamento_.dataPagamento)
+				, root.get(Lancamento_.valor), root.get(Lancamento_.tipo)
+				, root.get(Lancamento_.categoria).get(Categoria_.nome)
+				, root.get(Lancamento_.pessoa).get(Pessoa_.nome)));
+		
+		Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+		criteria.where(predicates);
+		
+		TypedQuery<ResumoLancamento> query = manager.createQuery(criteria);
+		adicionarRestricoesDePaginacao(query, pageable);
+		
+		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
+	}
+
+	private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
+			Root<Lancamento> root) {
 		List<Predicate> predicates = new ArrayList<>();
 		
 		if (!StringUtils.isEmpty(lancamentoFilter.getDescricao())) {
-			predicates.add(builder.like(builder.lower(root.get(Lancamento_.descricao)), "%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
+			predicates.add(builder.like(
+					builder.lower(root.get(Lancamento_.descricao)), "%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
 		}
 		
 		if (lancamentoFilter.getDataVencimentoDe() != null) {
-			predicates.add(builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoDe()));
+			predicates.add(
+					builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoDe()));
 		}
 		
 		if (lancamentoFilter.getDataVencimentoAte() != null) {
-			predicates.add(builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoAte()));
+			predicates.add(
+					builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoAte()));
 		}
 		
 		return predicates.toArray(new Predicate[predicates.size()]);
 	}
-	
-	private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+
+	private void adicionarRestricoesDePaginacao(TypedQuery<?> query, Pageable pageable) {
 		int paginaAtual = pageable.getPageNumber();
 		int totalRegistrosPorPagina = pageable.getPageSize();
 		int primeiroRegistroDaPagina = paginaAtual * totalRegistrosPorPagina;
@@ -77,6 +107,6 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 		
 		criteria.select(builder.count(root));
 		return manager.createQuery(criteria).getSingleResult();
-	}	
-
+	}
+	
 }
---
+++ src/main/resources/db/migration/V05__criar_e_registrar_usuarios_e_permissoes.sql
@@ -0,0 +1,65 @@
+CREATE SEQUENCE usuario_id_seq;
+CREATE TABLE usuario (
+	id bigint DEFAULT nextval('usuario_id_seq') NOT NULL,
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	senha VARCHAR(150) NOT NULL,
+	CONSTRAINT usuario_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE  public.usuario OWNER TO postgres;
+
+CREATE SEQUENCE permissao_id_seq;
+CREATE TABLE permissao (
+	id bigint DEFAULT nextval('permissao_id_seq') NOT NULL,
+	descricao VARCHAR(50) NOT NULL,
+	CONSTRAINT permissao_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE  public.permissao OWNER TO postgres;
+
+
+CREATE TABLE usuario_permissao (
+	id_usuario BIGINT NOT NULL,
+	id_permissao BIGINT NOT NULL,
+	CONSTRAINT usuario_permissao_pkey PRIMARY KEY (id_usuario, id_permissao),
+	CONSTRAINT usuario_fkey FOREIGN KEY ("id_usuario") REFERENCES "public"."usuario" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
+	CONSTRAINT permissao_fkey FOREIGN KEY ("id_permissao") REFERENCES "public"."permissao" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+) 
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE  public.usuario_permissao OWNER TO postgres;
+
+INSERT INTO usuario (nome, email, senha) values ('Administrador', 'admin@algamoney.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
+INSERT INTO usuario (nome, email, senha) values ('Maria Silva', 'maria@algamoney.com', '$2a$10$Zc3w6HyuPOPXamaMhh.PQOXvDnEsadztbfi6/RyZWJDzimE8WQjaq');
+
+INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_CATEGORIA');
+INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_CATEGORIA');
+
+INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_PESSOA');
+INSERT INTO permissao (descricao) values ('ROLE_REMOVER_PESSOA');
+INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_PESSOA');
+
+INSERT INTO permissao (descricao) values ('ROLE_CADASTRAR_LANCAMENTO');
+INSERT INTO permissao (descricao) values ('ROLE_REMOVER_LANCAMENTO');
+INSERT INTO permissao (descricao) values ('ROLE_PESQUISAR_LANCAMENTO');
+
+-- admin
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 1);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 2);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 3);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 4);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 5);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 6);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 7);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 8);
+
+-- maria
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (2, 2);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (2, 5);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (2, 8);
