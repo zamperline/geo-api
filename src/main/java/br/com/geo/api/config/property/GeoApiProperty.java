3751c0dc5e1b978c98810d904387b46da773e461
Roberval Zamperline <rzamperline@cambe.pr.gov.br>
2017-11-24 17:58:34 -0200

    producao

--- src/main/java/br/com/geo/api/config/property/GeoApiProperty.java
+++ src/main/java/br/com/geo/api/config/property/GeoApiProperty.java
@@ -5,8 +5,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("geo")
 public class GeoApiProperty {
 
+
 	private String originPermitida = "https://geo-municipio-ui.herokuapp.com";
 
+	
 	private final Seguranca seguranca = new Seguranca();
 
 	public Seguranca getSeguranca() {
